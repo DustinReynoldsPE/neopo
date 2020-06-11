83ce112d5150c4f17ccdc3dbb28435263ab6a09b
JuniorCode <jacob.alan.robinson10@gmail.com>
2020-06-11 17:21:20 -0400

    [skip ci]

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ directory="$(mktemp -d)"
 #trap "rm -rf '$directory'" 0 2 3 15
 
 git clone https://github.com/nrobinson2000/neopo.git "$directory"
-git checkout dev "$directory"
+git -C "$directory" checkout dev
 
 mv "$directory/dist/unix/bin" ~/.neopo
 mv "$directory/src" ~/.neopo
